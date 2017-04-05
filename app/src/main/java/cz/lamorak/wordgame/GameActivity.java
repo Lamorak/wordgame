229a2e52d3f257623ab693499e9349901c26d2be
Ondrej Vancak <lamorak@outlook.com>
2017-04-06 00:20:59 +0200

    Implement game activity layout

--- app/src/main/java/cz/lamorak/wordgame/GameActivity.java
+++ app/src/main/java/cz/lamorak/wordgame/GameActivity.java
@@ -5,12 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by ondrej on 5.4.2017.
  */
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView countdown;
+    private TextView wordOriginal;
+    private TextView wordGuess;
+    private Button wrongButton;
+    private Button correctButton;
 
     public static void call(final Context context) {
         final Intent intent = new Intent(context, GameActivity.class);
@@ -21,5 +29,11 @@ public class GameActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        countdown = (TextView) findViewById(R.id.countdown);
+        wordOriginal = (TextView) findViewById(R.id.word_original);
+        wordGuess = (TextView) findViewById(R.id.word_guess);
+        correctButton = (Button) findViewById(R.id.button_correct);
+        wrongButton = (Button) findViewById(R.id.button_wrong);
     }
 }
