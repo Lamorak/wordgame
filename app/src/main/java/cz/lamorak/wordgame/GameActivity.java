b682aa0f3f3f63612ae4bb667d9eff20d7f711c9
Ondrej Vancak <lamorak@outlook.com>
2017-04-06 00:25:11 +0200

    Implement word loading in game activity

--- app/src/main/java/cz/lamorak/wordgame/GameActivity.java
+++ app/src/main/java/cz/lamorak/wordgame/GameActivity.java
@@ -8,11 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.List;
+import cz.lamorak.wordgame.model.Word;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 /**
  * Created by ondrej on 5.4.2017.
  */
 
 public class GameActivity extends AppCompatActivity {
+
+    private CompositeDisposable disposables;
+    private List<Word> words;
 
     private TextView countdown;
     private TextView wordOriginal;
@@ -30,10 +39,32 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        disposables = new CompositeDisposable();
+
         countdown = (TextView) findViewById(R.id.countdown);
         wordOriginal = (TextView) findViewById(R.id.word_original);
         wordGuess = (TextView) findViewById(R.id.word_guess);
         correctButton = (Button) findViewById(R.id.button_correct);
         wrongButton = (Button) findViewById(R.id.button_wrong);
+
+        disposables.add(
+                WordGameApp.getServiceProvider()
+                        .getWordService()
+                        .loadWords()
+                        .doOnNext(Collections::shuffle)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(this::startGame)
+        );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disposables.clear();
+    }
+
+    private void startGame(final List<Word> words) {
+        this.words = words;
     }
 }
